a3dbb3caf0293090c223cf4a3f1343190948cdda
shakhist <sj4sjacob@gmail.com>
2022-11-13 22:51:49 -0500

    control stms

---
+++ src/test/java/com/JavaBasics/ControlStmts.java
@@ -0,0 +1,17 @@
+package com.JavaBasics;
+
+public class ControlStmts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=5;
+if (a>b)
+{
+	System.out.println("a is greater thab b");
+}
+else
+	System.out.println("a is less than b");
+	}
+
+}
