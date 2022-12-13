a127f7ad9530b3e1d5391a885e3be2fa00b648ec
shakhist <sj4sjacob@gmail.com>
2022-12-12 23:24:24 -0500

    12/12 changes

---
+++ src/test/java/com/pages/MultipleWindows1.java
@@ -0,0 +1,13 @@
+package com.pages;
+
+import org.openqa.selenium.By;
+
+public class MultipleWindows1 {
+	public static By newWindow=By.name("newbrowserwindow123");
+	public static By newMenu=By.xpath("//div[@id='site-navigation-wrap']/following-sibling::div/a");
+    public static By Aboutme=By.xpath("//ul[@id='menu-main-2']/li[4]/a");
+    public static By newMessWindow=By.name("newmessagewindow321");
+    public static By messText=By.tagName("body");
+    public static By newBrowserTab=By.name("newbrowsertab453");
+    public static By tabmenu=By.id("menu-item-6044");
+}
